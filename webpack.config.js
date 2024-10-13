fa175efc8183e43e7869d48235392d286cb41b1b
Vinicius Lourenzoni <viniciuslourenzoni0@gmail.com>
2024-10-13 20:59:42 -0300

    alter webpack locale

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   entry: path.resolve(__dirname, "src/main.js"),
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "public"),
   },
 
   devServer: {
